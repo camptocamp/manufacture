6c4223a3d70b7305485d4ea1e1a589d58175ddf5
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2023-12-05 12:37:40 +0100

    fixup! squash! [REF] mrp_multi_level: location management

--- mrp_multi_level/models/product_mrp_area.py
+++ mrp_multi_level/models/product_mrp_area.py
@@ -176,16 +176,16 @@ class ProductMRPArea(models.Model):
     def _compute_qty_available(self):
         for rec in self:
             rec.qty_available = rec.product_id.with_context(
-                location=rec.mrp_area_id.location_id.id
+                location=rec._get_locations().ids
             ).qty_available
 
     def _compute_supply_method(self):
         group_obj = self.env["procurement.group"]
         for rec in self:
-            proc_loc = rec.location_proc_id or rec.mrp_area_id.location_id
+            proc_loc = rec.location_proc_id or rec.location_id
             values = {
                 "warehouse_id": rec.mrp_area_id.warehouse_id,
-                "company_id": rec.mrp_area_id.company_id,
+                "company_id": rec.company_id,
             }
             rule = group_obj._get_rule(rec.product_id, proc_loc, values)
             if not rule:
